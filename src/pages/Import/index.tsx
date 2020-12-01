e13cfff8afe5ffc94dfbdb4aa3246359c23248b0
Edwin <eltoncavichioli@gmail.com>
2020-12-01 11:05:16 -0400

    completo

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,33 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles.length) return;
+
+    // estamos passando somente um arquivo pq nossa api recebe somente 1 por vez
+    // isso poderia ser melhorado para receber um array.
+    const fileProps = uploadedFiles[0];
+
+    data.append('file', fileProps.file, fileProps.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploads = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploads);
   }
 
   return (
